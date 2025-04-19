b140d69028424daf5acfff0c5266dd5310fd4a21
AngelineChase <costasanghelina69@gmail.com>
2025-04-19 04:47:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function openHome(){
+    window.location.href = 'index.html';
+}
+
+function openFashion() {
+    window.location.href = 'fashion.html';
+}
+
+function openPsychology() {
+    window.location.href = 'psychology.html';
+}
+
+function openBooks() {
+    window.location.href = 'books.html';
+}
+
+function openContacts(){
+    window.location.href = 'contacts.html';
+}
